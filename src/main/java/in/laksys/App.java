213f903c313590b56115b7c232cfca4e795e719c
laksys <laksys@gmail.com>
2021-08-05 10:05:06 +0530

    more methods addeded

--- src/main/java/in/laksys/App.java
+++ src/main/java/in/laksys/App.java
@@ -1,5 +1,7 @@
 package in.laksys;
 
+import java.util.stream.IntStream;
+
 public class App {
     public static int gcd(int m, int n){
         if( n == 0)
@@ -7,7 +9,24 @@ public class App {
         else
             return gcd(n, m%n);
     }
-    public static void main(String[] args) {
-        System.out.println( gcd(12,28));
+    public static boolean isPrime(int num){
+        return num > 1 &&
+                IntStream.rangeClosed(2, num/2)
+                        .noneMatch(e-> num % e == 0);
+    }
+    public static boolean isPerfect(int num){
+        return num == IntStream.range(1,num)
+                .filter(e-> num % e == 0)
+                .sum();
+    }
+    public static boolean isArmstrong(int num){
+        int temp = num, digit, sum=0;
+
+        while( temp > 0 ) {
+            digit = temp % 10;
+            temp = temp / 10;
+            sum += digit * digit * digit;
+        }
+        return num == sum;
     }
 }
--- src/test/java/in/laksys/AppTest.java
+++ src/test/java/in/laksys/AppTest.java
@@ -4,8 +4,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
+    public AppTest(){
+        System.out.println("AppTest constructed....");
+    }
     @Test
     public void firstTest(){
         Assert.assertEquals(4, App.gcd(12,28));
+    }
+    @Test
+    public void secondTest(){
+        Assert.assertTrue(App.isPrime(19));
     }
 }
